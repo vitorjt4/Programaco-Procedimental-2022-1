b5900dba4686fbf7248c70d28ddcf208b736d2f1
vitorjt4 <vitor.jts4@gmail.com>
2022-12-06 15:24:13 -0300

    Update minMax.c
    Just a few corrections

--- Laboratorio-03/minMax.c
+++ Laboratorio-03/minMax.c
@@ -2,45 +2,57 @@
  * Arquivo: minMax.c
  * Data criação: 04/12/2022
  * Autor: Victor José Teixeira da Silva
- * Identifies the largest and smallest number in the sequence by comparing it with its neighbors
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(){
-    int n, max, min = 0;
 
-    printf("Digite a quantidade de elementos que você deseja\n");
+    int n, maior, menor =0;
+    double *data;
+    printf("Digite a quantidade de elementos que você deseja: \n");
     scanf("%d", &n);
-    int dados[n];
 
-    for(int i=0; i < n; i++) {
-        printf("Seu %d numero:\t", i+1);
-        scanf("%d", dados+i);   
-    }
+    // Allocating memory for n elements
+    data = (double *)calloc(n, sizeof(double));
+    if (data == NULL) {
+    printf("Error!!! memory not allocated.");
+    exit(0);
+  }
 
-    max = dados[0];
-    min = dados[0];
-
-
-
-    for(int i=0; i < n; i++) {
-    if(dados[i] > max)
-        max = dados[i];
     
-    if (dados[i]< min)
-        min = dados[i]; 
+      // Storing numbers entered by the user.
+    for (int i = 0; i < n; ++i) {
+    printf("Digite o %d elemento: ", i + 1);
+    scanf("%lf", data + i);
+  }
+
+  maior = *data;
+  menor = *data;
+// tryint to locate the largest and smallest
+    for(int i=0; i < n; i++) {
+    if(*data > maior)
+        maior = *data;
+    
+    if (*data< menor)
+        menor = *data; 
     }
     
      for(int i=0; i < n; i++) {
-        if(dados[i] == max)
-            printf("%d > ", max);
-        if(dados[i] == min)
-            printf("%d < ", min);
-        if(dados[i] != max && dados[i] != min)
-            printf("%d ", dados[i]);
+        if(*data == maior)
+            printf("%d > ", maior);
+        if(*data == menor)
+            printf("%d < ", menor);
+        if(*data != maior && *data != menor)
+            printf("%.2lf ", *data);
     }
 
-return 0;
+    free(data);
+
+
+
+    return 0;
 }
