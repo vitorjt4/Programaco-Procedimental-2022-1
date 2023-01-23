706504a89acf287cc95f6bdac4ea587e96812daa
Victor José Teixeira <vitor.jts4@gmail.com>
2023-01-22 23:00:14 -0300

    Create txtFibonacci.c

---
+++ Laboratorio 07/txtFibonacci.c
@@ -0,0 +1,21 @@
+/*
+ * Arquivo: txtFibonacci.c
+ * Data criação: 22/jan/2023
+ * Autor: Victor José Teixeira da Silva
+ */
+
+#include <stdio.h>
+
+int main(){
+
+    int num = 0;
+    long long int a = 0;
+    
+    FILE * fib;
+
+    printf("Escolha o tamanho da sequencia de Fibonacci:\n");
+    scanf("%d%*c", &num);
+
+
+return 0;
+}
